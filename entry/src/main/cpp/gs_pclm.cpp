2f77d336bf9399d5f7302f2df2c28e811e748b9e
nulinuli0524 <105159045+nulinuli0524@users.noreply.github.com>
2025-07-07 14:07:48 +0800

    Update gs_pclm.cpp

--- entry/src/main/cpp/gs_pclm.cpp
+++ entry/src/main/cpp/gs_pclm.cpp
@@ -10,19 +10,20 @@ napi_value ConvertPdfToPclm(napi_env env, napi_callback_info info) {
   size_t argc = 2; napi_value argv[2];
   napi_get_cb_info(env, info, &argc, argv, nullptr, nullptr);
 
-  // 读取 inputPath
+  // input path
   size_t len;
   napi_get_value_string_utf8(env, argv[0], nullptr, 0, &len);
   std::string input(len+1, '\0');
   napi_get_value_string_utf8(env, argv[0], &input[0], len+1, nullptr);
 
-  // 读取 outputPath
+  // output path
   napi_get_value_string_utf8(env, argv[1], nullptr, 0, &len);
   std::string output(len+1, '\0');
   napi_get_value_string_utf8(env, argv[1], &output[0], len+1, nullptr);
 
-  std::string cmd = "./gs -q -dNOPAUSE -dBATCH -sDEVICE=pclm" +
-                    std::string(" -sOutputFile=\"") + output + "\" \"" + input + "\"";
+  // 调用同目录下的 gs
+  std::string cmd = "./gs -q -dNOPAUSE -dBATCH -sDEVICE=pclm"
+    + std::string(" -sOutputFile=\"") + output + "\" \"" + input + "\"";
   bool ok = Exec(cmd);
 
   napi_value result;
