571c93fd9c63564029b289930b0f595f93248208
nulinuli0524 <105159045+nulinuli0524@users.noreply.github.com>
2025-07-07 13:47:23 +0800

    Create gs_pclm.cpp

---
+++ entry/src/main/cpp/gs_pclm.cpp
@@ -0,0 +1,41 @@
+#include <napi/native_api.h>
+#include <cstdlib>
+#include <string>
+
+static bool Exec(const std::string &cmd) {
+  return (std::system(cmd.c_str()) == 0);
+}
+
+napi_value ConvertPdfToPclm(napi_env env, napi_callback_info info) {
+  size_t argc = 2; napi_value argv[2];
+  napi_get_cb_info(env, info, &argc, argv, nullptr, nullptr);
+
+  // 读取 inputPath
+  size_t len;
+  napi_get_value_string_utf8(env, argv[0], nullptr, 0, &len);
+  std::string input(len+1, '\0');
+  napi_get_value_string_utf8(env, argv[0], &input[0], len+1, nullptr);
+
+  // 读取 outputPath
+  napi_get_value_string_utf8(env, argv[1], nullptr, 0, &len);
+  std::string output(len+1, '\0');
+  napi_get_value_string_utf8(env, argv[1], &output[0], len+1, nullptr);
+
+  std::string cmd = "./gs -q -dNOPAUSE -dBATCH -sDEVICE=pclm" +
+                    std::string(" -sOutputFile=\"") + output + "\" \"" + input + "\"";
+  bool ok = Exec(cmd);
+
+  napi_value result;
+  napi_get_boolean(env, ok, &result);
+  return result;
+}
+
+napi_value Init(napi_env env, napi_value exports) {
+  napi_value fn;
+  napi_create_function(env, "convertPdfToPclm", NAPI_AUTO_LENGTH,
+                       ConvertPdfToPclm, nullptr, &fn);
+  napi_set_named_property(env, exports, "convertPdfToPclm", fn);
+  return exports;
+}
+
+NAPI_MODULE(NODE_GYP_MODULE_NAME, Init)
